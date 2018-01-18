04470bc9654de4495d3b5b1b3e7d96d314bf398e
wangjt <Wangjt0118@163.com>
2018-01-18 17:37:54 +0800

    Initial commit

---
+++ wuliao.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('jstest.html')
+
+
+@app.route('/addnumber')
+def add():
+    a = request.args.get('a', 0, type=float)
+    b = request.args.get('b', 0, type=float)
+    return jsonify({'result': a + b})
+
+
+from test import lantu
+app.register_blueprint(lantu, url_prefix='')
+
+if __name__ == "__main__":
+    print(app.url_map)
+    app.run(debug=True)
